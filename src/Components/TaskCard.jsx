39d281533fc7baf1ef9a10d18c4fb0d81727a7fe
Tom Byers <tmbyers1102@gmail.com>
2022-12-15 09:57:18 -0800

    test on taskcard

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -7,6 +7,7 @@ const TaskCard = ({task}) => {
 
                 <div className="bg-teal-600 h-36 rounded-lg p-3 hover:bg-teal-500">
                     <h2 className="m-3">{task.fields.Name}</h2>
+                    <p>testing push</p>
                 </div>
         </div>
     )
